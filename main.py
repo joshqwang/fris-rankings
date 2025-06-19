75ed6065a916fd7f8caf997b0989a28f6d282be6
joshqwang <63423092+joshqwang@users.noreply.github.com>
2025-06-19 17:27:51 -0500

    added code to get all teams in gender division

--- main.py
+++ main.py
@@ -1,35 +1,69 @@
 from usau_scraper import *
+import re
 import requests
 from bs4 import BeautifulSoup
 
-# Get a team's basic information
-
+INDIVIDUAL_TEAM_BASEURL = "https://play.usaultimate.org/teams/events/Eventteam/"
+class Team:
+    def __init__(self, college, name, gender, url):
+        self.college = college
+        self.name = name
+        self.gender = gender
+        self.url = url
+    def __str__(self):
+        return f"{self.college} ({self.name})\n{self.gender}'s division\nURL - {self.url}"
 
 # Get a team's schedule for the current season
 def fetch_teams(gender_division):
-    '''Args = Women or Men '''
+    '''Args = women or men '''
     url = 'https://play.usaultimate.org/teams/events/rankings/'
     teams = {}
     with requests.Session() as req:
         r = req.get(url)
         soup = BeautifulSoup(r.content, 'html.parser')
         data = {
-             "__EVENTTARGET": "CT_Main_0$gvList$ctl23$ctl00$ctl00",
-             'CT_Main_0$F_CompetitionLevelId' : "College"
+             "CT_Main_0$btnSubmit" : "Submit",
+             'CT_Main_0$F_CompetitionLevelId' : "College",
+             'CT_Main_0$F_GenderDivisionId' : ("17" if gender_division == "men" else "2")
                 }
         data['__VIEWSTATEGENERATOR'] = soup.find("input", id="__VIEWSTATEGENERATOR").get("value")
         data['__VIEWSTATE'] = soup.find("input", id="__VIEWSTATE").get("value")
         data['__EVENTVALIDATION'] = soup.find("input", id="__EVENTVALIDATION").get("value")
         r = req.post(url, data=data)
         soup = BeautifulSoup(r.content, 'html.parser')
-        print(data)
-        print(soup.prettify())
+        print_list(get_teams_on_page(soup, "men"))
+        del data["CT_Main_0$btnSubmit"] 
+        while True:
+            next_target = get_next_page(soup, data)
+            if not next_target:
+                break
+            data["__EVENTTARGET"] = next_target
 
-def generate_data(req):
+            data['__VIEWSTATEGENERATOR'] = soup.find("input", id="__VIEWSTATEGENERATOR").get("value")
+            data['__VIEWSTATE'] = soup.find("input", id="__VIEWSTATE").get("value")
+            data['__EVENTVALIDATION'] = soup.find("input", id="__EVENTVALIDATION").get("value")
+            r = req.post(url, data=data)
+            soup = BeautifulSoup(r.content, 'html.parser')
+            print_list(get_teams_on_page(soup, "men"))
 
-    r = req.get('https://play.usaultimate.org/teams/events/rankings/')
-    soup = BeautifulSoup(r.content, 'html.parser')
+
+def print_list(list):
+    for item in list:
+        print(item)
+def get_teams_on_page(soup, gender):
+    teams = []
+    for team in soup.find_all(href=re.compile('/teams/events/Eventteam/')):
+        url = team['href']
+        text_match = re.search("(.*) \((.*)\)", team.text)
+        college, name = text_match[1], text_match[2]
+        teams.append(Team(college, name, gender, url))
+    return teams
 
 
-    return data
-fetch_teams("Men")
+    #print(soup.find_all(href=re.compile("/teams/events/Eventteam/")))
+def get_next_page(soup, data):
+    href = soup.find("a", string=re.compile("Next 20"))
+    if not href:
+        return None
+    return re.search("\('(.*?)'", href['href']).group(1)
+fetch_teams("men")
